f2916eac3632f3db90675861d52e3cfd80c58b29
subbaSahil <sahil_s@pursuitsoftware.com>
2025-02-13 18:34:56 +0530

    new push

---
+++ src/main/java/Helper/BaseTest.java
@@ -0,0 +1,40 @@
+package Helper;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+    
+    private Properties properties = new Properties();
+    
+    private void loadProperties() {
+    	try(FileInputStream fileInput = new FileInputStream("C:\\Users\\RashmiKumari\\eclipse-workspace\\pursuit_Software\\config.properties")){
+    		properties.load(fileInput);
+    	}catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    @BeforeMethod
+    public void setup() {
+//    	driver = new ChromeDriver();
+    	driver = new EdgeDriver();
+    	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    	loadProperties();
+    	driver.get(properties.getProperty("URL"));
+    	driver.manage().window().maximize();
+    	wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    }
+        
+}
+
